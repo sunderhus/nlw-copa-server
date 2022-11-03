641e500ffc05aee04d0a462456dffc156d6df4d0
Sunderhus <matheus.sunderhus@gmail.com>
2022-11-03 16:24:19 -0300

    feat: add pools create route with schema validation

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import {PrismaClient} from 'prisma/prisma-client'
-
+import crypto from 'crypto';
+import{z} from 'zod'
 
 const prisma = new PrismaClient({
     log:['query']
@@ -33,6 +34,24 @@ async function bootStrap(){
 
         return {count}
     });
+
+    fastify.post('/pools',async (request,reply)=>{
+        const createPoolBody = z.object({
+            title:z.string(),
+        });
+
+        const {title} = createPoolBody.parse(request.body);
+        const code = crypto.randomUUID().toUpperCase();
+        await prisma.pool.create({
+            data:{
+                code,
+                title,
+            }
+        })
+
+
+        return reply.status(201).send({code})
+    })
 
     await fastify.listen({port:3333, /*host:'0.0.0.0'*/});
 }
