f21c213b5b88044ad2a80b51dedc6daddec9683f
Sunderhus <matheus.sunderhus@gmail.com>
2022-11-18 09:45:54 -0300

    feat: register fastify-env to server file

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,23 @@ import jwt from '@fastify/jwt';
 import Fastify from 'fastify'
 import { FastifyInstance } from 'fastify';
 import { pollRoutes,guessRoutes,userRoutes, authRoutes, gameRoutes} from '@/routes';
+import fastifyEnv from '@fastify/env'
 
 type Route = (fastify: FastifyInstance)=> Promise<void>;
-
 interface Params {
     routes:Route[];
     fastify: FastifyInstance;
 }
+
+const envSchema = {
+    type:'object',
+    properties:{
+        SECRET:{
+            type:'string'
+        }
+    }
+}
+
 
 const registerRoutes =  async({fastify,routes}:Params)=>{
     routes.forEach(async (route) => {
@@ -22,14 +32,19 @@ async function bootStrap(){
         logger:true,
     });
 
+    await fastify.register(fastifyEnv,{
+        confKey:'config',
+        schema:envSchema
+    })
+
     await fastify.register(cors,{
         origin:true
     });
 
     await fastify.register(jwt,{
-        secret:'NLWCopaSecret',
+        secret:`${fastify.config.SECRET}`,
     });
-    
+
     await registerRoutes({
         fastify,
         routes:[
